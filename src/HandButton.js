7417bf11d3d5b2fd51f4b1a25da01546c91ea779
Gaeul Jun <gaeul7000@naver.com>
2025-06-14 00:31:37 +0900

    made click functions

---
+++ src/HandButton.js
@@ -0,0 +1,11 @@
+import HandIcon from "./HandIcon";
+
+function HandButton({value, onClick}) {
+    // 개 미쳐도르 handleClick에서 위에 인자로 받은 onClick에 value를 넘겨주는것;;
+    const handleClick = () => onClick(value);
+    return (<button onClick={handleClick}>
+                <HandIcon value={value}/>
+            </button>)
+}
+
+export default HandButton;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import ReactDOM from 'react-dom/client';
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-        <h1 id="title">가위바위보</h1>
-        <h2>{getResult()}</h2>
-        <button className="hand" onClick={handleClick}>가위</button>
-        <button className="hand" onClick={handleClick}>바위</button>
-        <button className="hand" onClick={handleClick}>보</button>
+        <App />
     </>
 );
-
-function getResult(me, other){
-    return "승리";
-}
-
-function handleClick(){
-    console.log("가위바위보!");
-}
