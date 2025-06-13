373a772396f48ce6d9d7ccf4d086a149b93f09e3
gazarri <gaeul700@gmail.com>
2025-06-14 00:41:58 +0900

    change email

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,7 +1,7 @@
 import HandIcon from "./HandIcon";
 
 function HandButton({value, onClick}) {
-    // 개 미쳐도르 handleClick에서 위에 인자로 받은 onClick에 value를 넘겨주는것;;
+    // 개 미쳐도르 handleClick에서 위에 인자로 받은 onClick에 value를 넘겨주는것;
     const handleClick = () => onClick(value);
     return (<button onClick={handleClick}>
                 <HandIcon value={value}/>
